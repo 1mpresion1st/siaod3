31e3d83fc1225b4198e27940a2dc53ee0a5a235a
willy <tokarev007007@gmail.com>
2023-09-19 16:58:58 +0300

    final update on first prac

--- pracs/prac1/1с.cpp
+++ pracs/prac1/1с.cpp
@@ -1,12 +1,9 @@
 #include <cstdlib>
 #include <iostream>
-//#include <Windows.h>
 #include <bitset>
 using namespace std;
 
 int main() {
-//    SetConsoleCP(1251);
-//    SetConsoleOutputCP(1251);
     unsigned int x = 25;
     const int n = sizeof(int) * 8; // Количество разрядов в числе типа int (обычно 32)
     unsigned maska = (1 << (n - 1)); // Устанавливаем 1 в старшем бите 32-разрядной сетки
@@ -21,7 +18,5 @@ int main() {
         maska = maska >> 1; // Сдвигаем 1 в маске на один разряд вправо
     }
     cout << endl;
-
-//    system("pause");
     return 0;
 }
